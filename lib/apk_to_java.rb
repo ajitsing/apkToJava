ffc3f56b96349af23a25849447f0bb499e1a0802
AjitSingh <jeetsingh.ajit@gmail.com>
2016-01-30 13:07:43 +0530

    adding validations and some refactoring

--- lib/apk_to_java.rb
+++ lib/apk_to_java.rb
@@ -19,11 +19,11 @@ module ApkToJava
     end
 
     def jadx_installed?
-      !`ls #{ApkToJava::JADX}`.empty?
+      File.exists? ApkToJava::JADX
     end
 
     def dex_to_jar_installed?
-      !`ls #{ApkToJava::DEX_TO_JAR}`.empty?
+      File.exists? ApkToJava::DEX_TO_JAR
     end
 
     def env_setup?
@@ -39,53 +39,68 @@ module ApkToJava
   end
 
   module Operations
-    def convert_to_dex(apk)
-      print_info "Converting to dex.."
-      `mkdir apkToJava`
-      `cp #{apk} apkToJava/`
-      apk_name = apk.split('/').last.split('.').first
-      apk = "apkToJava/#{apk_name}"
-      `mv apkToJava/#{apk_name}.apk apkToJava/#{apk_name}.zip`
-      `unzip apkToJava/#{apk_name}.zip -d apkTojava/`
-      `#{ApkToJava::DEX_TO_JAR} apkTojava/classes.dex`
-      print_success "Done!"
-      `pwd`.chomp + "/apkTojava/classes.dex"
+    TEMP_DIR = 'apkToJavaTmp'
+
+    def copy_apk(apk)
+      `mkdir #{TEMP_DIR}`
+      `cp #{apk} #{TEMP_DIR}/`
     end
 
-    def dex_to_jar(apk)
-      remove_existing_jar if jar_exists?
-      dex_file = convert_to_dex apk
-      print_info "Converting dex to jar.."
-      command = ApkToJava::DEX_TO_JAR + " #{dex_file}"
-      `#{command}`
-      print_success "Done!"
+    def unzip(apk_name)
+      `mv #{TEMP_DIR}/#{apk_name}.apk #{TEMP_DIR}/#{apk_name}.zip`
+      `unzip #{TEMP_DIR}/#{apk_name}.zip -d #{TEMP_DIR}/`
+    end
+
+    def create_dex
+      `#{ApkToJava::DEX_TO_JAR} #{TEMP_DIR}/classes.dex`
+    end
+
+    def path_to_dex
+      `pwd`.chomp + "/#{TEMP_DIR}/classes.dex"
+    end
+
+    def create_jar(dex_file)
+      `#{ApkToJava::DEX_TO_JAR} #{dex_file} --force`
+    end
+
+    def path_to_jar
       `pwd`.chomp + '/classes-dex2jar.jar'
-    end
-
-    def jar_exists?
-      print `ls #{`pwd`.chomp}/classes-dex2jar.jar`
-      !`ls #{`pwd`.chomp}/classes-dex2jar.jar`.empty?
-    end
-
-    def remove_existing_jar
-      print_info "Removing existing jar.."
-      `rm #{`pwd`.chomp}/classes-dex2jar.jar`
     end
 
     def clean_up
       print_info "Cleaning the mess.."
-      `rm -rf apkToJava`
+      `rm -rf #{TEMP_DIR}`
       print_success "done!"
+    end
+
+    def open_code_in_gui(jar_file)
+      `#{ApkToJava::JADX}  #{jar_file} &`
+    end
+
+    def convert_to_dex(apk)
+      print_info "Converting to dex.."
+      copy_apk apk
+      apk_name = apk.split('/').last.split('.').first
+      unzip apk_name
+      create_dex
+      print_success "Done!"
+      path_to_dex
+    end
+
+    def dex_to_jar(apk)
+      dex_file = convert_to_dex apk
+      print_info "Converting dex to jar.."
+      create_jar dex_file
+      print_success "Done!"
+      path_to_jar
     end
 
     def view_as_java_code(apk)
       jar_file = dex_to_jar apk
       clean_up
       print_info "Opening in gui interface.."
-
-      command = ApkToJava::JADX + " #{jar_file} &"
       print_success "Please be patient, apkToJava might take some time to load your project in gui interface"
-      `#{command}`
+      open_code_in_gui jar_file
     end
   end
 end
