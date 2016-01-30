e061a20609129042bc3b70d928cccefe4328dedf
AjitSingh <jeetsingh.ajit@gmail.com>
2016-01-30 11:09:02 +0530

    remove existing jar

--- lib/apk_to_java.rb
+++ lib/apk_to_java.rb
@@ -7,15 +7,15 @@ module ApkToJava
 
   module Setup
     def install_dex2jar
-      print_info("installing dex2jar..")
+      print_info("Installing dex2jar..")
       `ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null && brew install dex2jar`
-      print_success "done!"
+      print_success "Done!"
     end
 
     def install_jadx
-      print_info("installing jadx..")
+      print_info("Installing jadx..")
       `cd /usr/local/Cellar && wget https://github.com/skylot/jadx/releases/download/v0.6.0/jadx-0.6.0.zip && unzip jadx-0.6.0.zip -d jadx/ && rm jadx-0.6.0.zip && cd -`
-      print_success "done!"
+      print_success "Done!"
     end
 
     def jadx_installed?
@@ -34,7 +34,7 @@ module ApkToJava
       print_info("Initializing setup!!")
       install_dex2jar if !dex_to_jar_installed?
       install_jadx if !jadx_installed?
-      print_success("setup done :)")
+      print_success("Setup done :)")
     end
   end
 
@@ -48,21 +48,32 @@ module ApkToJava
       `mv apkToJava/#{apk_name}.apk apkToJava/#{apk_name}.zip`
       `unzip apkToJava/#{apk_name}.zip -d apkTojava/`
       `#{ApkToJava::DEX_TO_JAR} apkTojava/classes.dex`
-      print_success "done!"
+      print_success "Done!"
       `pwd`.chomp + "/apkTojava/classes.dex"
     end
 
     def dex_to_jar(apk)
+      remove_existing_jar if jar_exists?
       dex_file = convert_to_dex apk
-      print_info "Converting dex to jar"
+      print_info "Converting dex to jar.."
       command = ApkToJava::DEX_TO_JAR + " #{dex_file}"
       `#{command}`
-      print_success "done!"
+      print_success "Done!"
       `pwd`.chomp + '/classes-dex2jar.jar'
     end
 
+    def jar_exists?
+      print `ls #{`pwd`.chomp}/classes-dex2jar.jar`
+      !`ls #{`pwd`.chomp}/classes-dex2jar.jar`.empty?
+    end
+
+    def remove_existing_jar
+      print_info "Removing existing jar.."
+      `rm #{`pwd`.chomp}/classes-dex2jar.jar`
+    end
+
     def clean_up
-      print_info "cleaning the mess.."
+      print_info "Cleaning the mess.."
       `rm -rf apkToJava`
       print_success "done!"
     end
@@ -73,7 +84,7 @@ module ApkToJava
       print_info "Opening in gui interface.."
 
       command = ApkToJava::JADX + " #{jar_file} &"
-      print_success "Please be patience, apkToJava might take some time to load your project in gui interface"
+      print_success "Please be patient, apkToJava might take some time to load your project in gui interface"
       `#{command}`
     end
   end
